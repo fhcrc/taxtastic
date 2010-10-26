4522a22277e6c4d9337bde01e09599491c7f34c6
Brian Hodges <bhodges@fhcrc.org>
2010-10-26 13:04:16 -0700

    Added MLOutputParser class to package.py.  Also added related testfiles.

--- taxonomy/package.py
+++ taxonomy/package.py
@@ -65,3 +65,185 @@ def create(pkg_dir, options, manifest_name=manifest_name, package_contents=packa
     write_config(fname=manifest, optdict=optdict, sections=package_contents)
 
 
+
+
+
+# MLOutputParser class - parse maximum likelihood output files generated 
+# by RAxML (2 types) and PhyML.
+#
+# Supported file types include:
+#     raxml_condensed - rel_rates on a single line
+#     raxml_re_estimated - rel_rates 1 per line
+#     phyml_dna
+#     phyml_aa
+
+import os
+import re
+import json
+
+
+class MLOutputParser:
+    """Maximum Likelihood Output Parser Class"""
+    
+    file_name = '' # Name of file to parse.
+    file_type = 'unknown'  # Type of file examined, defaults to unknown. 
+    input_text = '' # Full text of input file.
+    # Property of the class that gets populated when matches are found 
+    # within an output file.
+    ml_values = { 'program' : '',
+                  'datatype' : '',
+                  'matrix' : '',
+                  'gamma_cat' : 4, # Default is 4.
+                  'gamma_alpha' : None, # This is actually a Decimal, but Decimal is not JSON serializable so we just work with a string.
+                  'rel_rates' : { 'ac' : None, #  rel_rates are actually Decimals, but Decimal is not JSON serializable so we just work with a string.
+                                  'ag' : None, 
+                                  'at' : None,
+                                  'cg' : None,
+                                  'ct' : None,
+                                  'gt' : None                                                        
+                                }
+                 }
+    
+### Public methods ###    
+    
+    # Return contents of ml_values, in the format of a dictionary.
+    def get_ml_values(self):
+        return self.ml_values
+    
+    # Return contents of ml_values in JSON format.
+    def get_ml_json(self):
+        return json.dumps(self.ml_values)
+    
+    
+    # Return contents of file_type.
+    def get_file_type(self):
+        return self.file_type
+     
+    # Extract data from ML output files.
+    def parse_ml_data(self):    
+        # Read input file into a string for multiline matching.
+        try:
+            input_file = open(self.file_name, 'r')
+            self.input_text = input_file.read()
+            input_file.close()
+        except IOError as (errno, strerror):
+            print "I/O error({0}): {1}".format(errno, strerror)
+            raise
+        match_found = False
+        if (self._parse_raxml_condensed()):
+            self.file_type = 'raxml_condensed'
+            match_found = True
+        elif (self._parse_raxml_re_estimated()):
+            self.file_type = 'raxml_re_estimated'
+            match_found = True            
+        elif (self._parse_phyml_dna()):
+            self.file_type = 'phyml_dna'
+            match_found = True       
+        elif (self._parse_phyml_aa()):
+            self.file_type = 'phyml_aa'
+            match_found = True   
+        return match_found    
+    
+    # Determine the md5sum of the output file.
+    def get_md5sum(self):
+        pass
+    
+### Private methods ###    
+    
+    # Parse RAxML info file - condensed.    
+    def _parse_raxml_condensed(self):
+        try:
+            regex = re.compile('.*(RAxML version .*?) release.*DataType: (\w+).*Substitution Matrix: (\w+).*alpha\[0\]: (\d+\.\d+) rates\[0\] ac ag at cg ct gt: (\d+\.\d+) (\d+\.\d+) (\d+\.\d+) (\d+\.\d+) (\d+\.\d+) (\d+\.\d+) ',
+                               re.M|re.DOTALL)
+            if (regex.match(self.input_text)):
+                self.ml_values['program'] = regex.match(self.input_text).group(1) # program
+                self.ml_values['datatype'] = regex.match(self.input_text).group(2) # datatype
+                self.ml_values['matrix'] = regex.match(self.input_text).group(3) # matrix
+                self.ml_values['gamma_alpha'] = str(regex.match(self.input_text).group(4)) # gamma_alpha
+                self.ml_values['rel_rates']['ac'] = str(regex.match(self.input_text).group(5)) # ac
+                self.ml_values['rel_rates']['ag'] = str(regex.match(self.input_text).group(6)) # ag
+                self.ml_values['rel_rates']['at'] = str(regex.match(self.input_text).group(7)) # at
+                self.ml_values['rel_rates']['cg'] = str(regex.match(self.input_text).group(8)) # cg
+                self.ml_values['rel_rates']['ct'] = str(regex.match(self.input_text).group(9)) # ct
+                self.ml_values['rel_rates']['gt'] = str(regex.match(self.input_text).group(10)) # gt
+                return True       
+            else:
+                return False   
+        except:
+            return False
+
+    # Parse RAxML info file - re-estimated.
+    def _parse_raxml_re_estimated(self):
+        regex = re.compile('.*(RAxML version .*?) release.*DataType: (\w+).*Substitution Matrix: (\w+).*alpha: (\d+\.\d+).*rate A <-> C: (\d+\.\d+).*rate A <-> G: (\d+\.\d+).*rate A <-> T: (\d+\.\d+).*rate C <-> G: (\d+\.\d+).*rate C <-> T: (\d+\.\d+).*rate G <-> T: (\d+\.\d+)', 
+                           re.M|re.DOTALL)
+        try:
+            if (regex.match(self.input_text)):
+                self.ml_values['program'] = regex.match(self.input_text).group(1) # program
+                self.ml_values['datatype'] = regex.match(self.input_text).group(2) # datatype
+                self.ml_values['matrix'] = regex.match(self.input_text).group(3) # matrix
+                self.ml_values['gamma_alpha'] = str(regex.match(self.input_text).group(4)) # gamma_alpha
+                self.ml_values['rel_rates']['ac'] = str(regex.match(self.input_text).group(5)) # ac
+                self.ml_values['rel_rates']['ag'] = str(regex.match(self.input_text).group(6)) # ag
+                self.ml_values['rel_rates']['at'] = str(regex.match(self.input_text).group(7)) # at
+                self.ml_values['rel_rates']['cg'] = str(regex.match(self.input_text).group(8)) # cg
+                self.ml_values['rel_rates']['ct'] = str(regex.match(self.input_text).group(9)) # ct
+                self.ml_values['rel_rates']['gt'] = str(regex.match(self.input_text).group(10)) # gt
+                return True       
+            else:
+                return False   
+        except:
+            return False
+     
+    # Parse PhyML output file - DNA
+    def _parse_phyml_dna(self):
+        regex = re.compile('.*---\s+(PhyML .*?)\s+ ---.*Model of .*? substitution:\s+(\w+).*Number of categories:\s+(\d+).*Gamma shape parameter:\s+(\d+\.\d+).*A <-> C\s+(\d+\.\d+).*A <-> G\s+(\d+\.\d+).*A <-> T\s+(\d+\.\d+).*C <-> G\s+(\d+\.\d+).*C <-> T\s+(\d+\.\d+).*G <-> T\s+(\d+\.\d+)', 
+                           re.M|re.DOTALL)
+        re_datatype = re.compile('.*Nucleotides frequencies.*', re.M|re.DOTALL)
+        try:
+            if (regex.match(self.input_text)):
+                self.ml_values['program'] = regex.match(self.input_text).group(1) # program
+                self.ml_values['matrix'] = regex.match(self.input_text).group(2) # matrix            
+                self.ml_values['gamma_cat'] =  regex.match(self.input_text).group(3) # gamma_cat
+                self.ml_values['gamma_alpha'] = str(regex.match(self.input_text).group(4)) # gamma_alpha
+                self.ml_values['rel_rates']['ac'] = str(regex.match(self.input_text).group(5)) # ac
+                self.ml_values['rel_rates']['ag'] = str(regex.match(self.input_text).group(6)) # ag
+                self.ml_values['rel_rates']['at'] = str(regex.match(self.input_text).group(7)) # at
+                self.ml_values['rel_rates']['cg'] = str(regex.match(self.input_text).group(8)) # cg
+                self.ml_values['rel_rates']['ct'] = str(regex.match(self.input_text).group(9)) # ct
+                self.ml_values['rel_rates']['gt'] = str(regex.match(self.input_text).group(10)) # gt
+                self.ml_values['datatype'] = 'DNA' # datatype                
+                return True       
+            else:
+                return False   
+        except:
+            return False  
+
+    # Parse PhyML output file - AA
+    def _parse_phyml_aa(self):
+        regex = re.compile('.*---\s+(PhyML .*?)\s+ ---.*Model of .*? substitution:\s+(\w+).*', 
+                           re.M|re.DOTALL)
+
+        try:
+            if (regex.match(self.input_text)):
+                self.ml_values['program'] = regex.match(self.input_text).group(1) # program
+                self.ml_values['matrix'] = regex.match(self.input_text).group(2) # matrix            
+                self.ml_values['datatype'] = 'AA' # datatype   
+                self.ml_values['gamma_cat'] = None # N/A 
+                return True       
+            else:
+                return False   
+        except:
+            return False  
+
+            
+### Constructor ###
+            
+    def __init__(self, file_name):
+        self.file_name = file_name
+        # Make sure the file passed in both exists and is readable.
+        try:            
+            if (not os.access(self.file_name, os.R_OK)):
+                raise Exception()
+        except:
+            print 'File is not readable: ' + file_name
+            sys.exit(1)
