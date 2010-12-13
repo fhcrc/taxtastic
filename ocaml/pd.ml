db194089e1cde5cdf43b538ffc67504dcfbf952a
Erick Matsen <matsen@fhcrc.org>
2010-12-13 05:15:14 -0800

    split into ptree and pd

--- ocaml/pd.ml
+++ ocaml/pd.ml
@@ -1,181 +1,8 @@
-
-exception Found_multiply
-exception Other_side of int list * int list * int list 
-exception Side_without of int * int list * int list 
-exception Int_not_found of int
-exception Not_implemented of string
+(* taxtastic v0.1. Copyright (C) 2009-2010  Frederick A Matsen. 
+ * This file is part of taxtastic. Taxtastic is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. Taxtastic is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with taxtastic.  If not, see <http://www.gnu.org/licenses/>.  *)
 
 open MapsSets
-
-(* remove exactly one element from l; fail if found multiply *)
-let list_remove1 x l = 
-  match List.partition ((=) x) l with
-  | ([_],l') -> l'
-  | ([],_) -> raise (Int_not_found x)
-  | (_,_) -> raise Found_multiply
-
-let safe_add h k v = 
-  assert(not (Hashtbl.mem h k));
-  Hashtbl.add h k v
-
-
-(* *** PTREE BUILDING *** *)
-
-type edge = 
-  (*        id    bl      connections *)
-  | Pend of int * float * int list
-  | Inte of       float * int list * int list
-
-  (* oh yes! *)
-type ptree = (int, edge) Hashtbl.t
-
-let sorted_list_eq l1 l2 = List.sort compare l1 = List.sort compare l2
-
-(* assert that one_side is a (potentially resorted version of) l or r, and
- * return the one that it is not *)
-let get_other_side one_side l r =
-  if sorted_list_eq one_side l then r
-  else if sorted_list_eq one_side r then l
-  else raise (Other_side (one_side, l, r))
-
-let get_side_without x l r = 
-  if List.mem x l then r
-  else if List.mem x r then l
-  else raise (Side_without (x, l, r))
-
-let list_replace ~src ~dst = List.map (fun x -> if x = src then dst else x)
-let edgel_replace ~src ~dst = function
-  | Pend(id,bl,l) as p -> 
-      if List.mem src l then Pend(id,bl, list_replace src dst l) else p
-  | Inte(bl,l,r) as i -> 
-      if not ((List.mem src l) || (List.mem src r)) then i
-      else Inte(bl, list_replace src dst l, list_replace src dst r)
-
-let of_stree bl_getter st = 
-  let pt = Hashtbl.create (1+(Stree.max_id st)) in
-  let add_edge id left righto = 
-    let bl = bl_getter id in
-    Hashtbl.add pt id
-      (match righto with
-      | None -> Pend(id, bl, left)
-      | Some right -> Inte(bl, left, right))
-  in
-  let rec aux above_ids = function
-    | Stree.Node(id, tL) ->
-        let ids_of_tl = List.map Stree.top_id in
-        add_edge id above_ids (Some (ids_of_tl tL));
-        List.iter
-          (fun (below, rest) -> aux (id::(ids_of_tl rest)) below) 
-          (Base.pull_each_out tL)
-    | Stree.Leaf id -> add_edge id above_ids None
-  in
-  let root_build (to_build, rest) = aux (List.map Stree.top_id rest) to_build in
-  let () = 
-    match st with
-    | Stree.Leaf _ -> ()
-    | Stree.Node(_, [t1; t2]) -> begin
-      List.iter root_build (Base.pull_each_out [t1; t2]);
-      let (eid1, eid2) = (Stree.top_id t1, Stree.top_id t2) in
-      match ((eid1,Hashtbl.find pt eid1), (eid2,Hashtbl.find pt eid2)) with
-      | ((id1, Inte(bl1,l1,r1)), (id2, Inte(bl2,l2,r2))) -> 
-          (* remove degree two node at root *)
-          let join1 = get_other_side [id2] l1 r1
-          and join2 = get_other_side [id1] l2 r2
-          in
-          (* make new internal edge *)
-          Hashtbl.replace pt id1 (Inte(bl1+.bl2, join1, join2));
-          (* clean out old edge *)
-          Hashtbl.remove pt id2;
-          (* reconnect things to new edge *)
-          List.iter 
-            (fun id -> 
-              Hashtbl.replace pt id 
-                (edgel_replace ~src:id2 ~dst:id1 (Hashtbl.find pt id)))
-            (join1 @ join2);
-      | _ -> raise (Not_implemented "rooted on pendant edge")
-      end
-    | Stree.Node(_, tL) -> List.iter root_build (Base.pull_each_out tL);
-  in
-  pt
-
-let of_gtree gt = 
-  let get_bl i = 
-    try (IntMap.find i gt.Gtree.bark_map)#get_bl with
-    | Not_found -> failwith "tree is missing branch lengths"
-  in
-  of_stree get_bl gt.Gtree.stree
-
-let of_string s = of_gtree (Newick.of_string s)
-let of_file s = of_gtree (Newick.of_file s)
-
-
-(* uuuuuugly! *)
-exception Found_inte of int
-let find_internal pt = 
-  try
-    Hashtbl.iter 
-      (fun i -> function | Inte(_,_,_) -> raise (Found_inte i) | _ -> ())
-      pt;
-    None
-  with
-  | Found_inte i -> Some i
-
-let to_gtree pt = 
-  (* start with an index bigger than anything in pt *)
-  let count = ref (Hashtbl.fold (fun i _ -> max i) pt 0) 
-  and m = ref IntMap.empty
-  in
-  let add_bark i bl nameo = 
-    m := 
-      IntMap.add i 
-        (new Newick_bark.newick_bark (`Of_bl_name_boot(Some bl, nameo, None)))
-        !m
-  in
-  let rec aux ~bad our_id = 
-    match Hashtbl.find pt our_id with
-    | Inte(bl,l,r) ->
-        incr count;
-        add_bark (!count) bl None;
-        let our_side = get_side_without bad l r in
-        Stree.Node(!count, List.map (aux ~bad:our_id) our_side)
-    | Pend(id,bl,_) -> add_bark id bl (Some (string_of_int id)); Stree.Leaf id
-  in
-  match find_internal pt with
-  | Some start_edge -> begin
-      match Hashtbl.find pt start_edge with
-      | Inte(bl,l,r) -> 
-          let stl = aux ~bad:(List.hd r) start_edge
-          and str = aux ~bad:(List.hd l) start_edge
-          in
-          add_bark (Stree.top_id stl) (bl/.2.) None;
-          add_bark (Stree.top_id str) (bl/.2.) None;
-          incr count;
-          Gtree.gtree (Stree.Node(!count, [stl;str])) !m
-      | Pend(_,_,_) -> assert(false)
-  end
-  | None ->
-      let st = 
-        Stree.Node 
-          (1 + !count,
-          Hashtbl.fold
-            (fun _ e l ->
-              match e with 
-              | Inte(_,_,_) -> assert false
-              | Pend(id,bl,_) -> 
-                  add_bark id bl (Some (string_of_int id));
-                  (Stree.Leaf id)::l)
-            pt
-            [])
-      in
-      Gtree.gtree st !m
-
-let to_stree pt = Gtree.get_stree (to_gtree pt)
-
-(* *** PTREE CHANGING *** *)
-
-
-(* replace the binding of k in h with its image under f *)
-let hashtbl_map1 f h k = Hashtbl.replace h k (f (Hashtbl.find h k))
+open Ptree
 
 let freplace f h id = Hashtbl.replace h id (f (Hashtbl.find h id))
 
@@ -204,9 +31,9 @@ let idblset_of_pt pt =
 let delete_pend pt del_id idbls = 
   match Hashtbl.find pt del_id with
   | Inte(_,_,_) -> failwith "can't delete internal edge"
-  | Pend(_, _, el) -> 
+  | Pend(_, _, eidl) -> 
       Hashtbl.remove pt del_id;
-      (match el with
+      (match eidl with
       | [] | [_] -> assert false 
       | [eid1; eid2] -> begin
         (* degree two-- heal the wound. *)
@@ -259,12 +86,12 @@ let perform orig_pt stopping_bl =
   let rec aux accu s = 
     let m = IdblSet.min_elt s in
     if m.bl > stopping_bl then accu
-    else begin
-      Printf.printf "%d\t%g\n" m.id m.bl;
-      let x = Hashtbl.find pt m.id in
-      let new_s = delete_pend pt m.id (IdblSet.remove m s) in 
-      aux ((x,m.bl,to_stree pt)::accu) new_s
-    end
+    else match Hashtbl.find pt m.id with
+    | Pend(orig_id, bl, _) ->
+        assert(bl = m.bl);
+        let new_s = delete_pend pt m.id (IdblSet.remove m s) in 
+        aux ((orig_id,m.bl,to_stree pt)::accu) new_s
+    | Inte(_,_,_) -> assert false
   in
   List.rev (aux [] (idblset_of_pt pt))
 
---
+++ ocaml/ptree.ml
@@ -0,0 +1,183 @@
+(* taxtastic v0.1. Copyright (C) 2009-2010  Frederick A Matsen. 
+ * This file is part of taxtastic. Taxtastic is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. Taxtastic is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with taxtastic.  If not, see <http://www.gnu.org/licenses/>.  *)
+
+exception Other_side of int list * int list * int list 
+exception Side_without of int * int list * int list 
+exception Not_implemented of string
+
+open MapsSets
+
+type edge = 
+  (*        id    bl      connections *)
+  | Pend of int * float * int list
+  | Inte of       float * int list * int list
+
+(* oh yes! a mutable data structure, just for fun. *)
+type ptree = (int, edge) Hashtbl.t
+
+
+(* *** GENERALITIES *** *)
+
+let safe_add h k v = 
+  assert(not (Hashtbl.mem h k));
+  Hashtbl.add h k v
+
+let sorted_list_eq l1 l2 = List.sort compare l1 = List.sort compare l2
+
+(* assert that one_side is a (potentially resorted version of) l or r, and
+ * return the one that it is not *)
+let get_other_side one_side l r =
+  if sorted_list_eq one_side l then r
+  else if sorted_list_eq one_side r then l
+  else raise (Other_side (one_side, l, r))
+
+(* same, but for a single element *)
+let get_side_without x l r = 
+  if List.mem x l then r
+  else if List.mem x r then l
+  else raise (Side_without (x, l, r))
+
+let list_replace ~src ~dst = List.map (fun x -> if x = src then dst else x)
+
+let edgel_replace ~src ~dst = function
+  | Pend(id,bl,l) as p -> 
+      if List.mem src l then Pend(id,bl, list_replace src dst l) else p
+  | Inte(bl,l,r) as i -> 
+      if not ((List.mem src l) || (List.mem src r)) then i
+      else Inte(bl, list_replace src dst l, list_replace src dst r)
+
+
+(* *** OF *** *)
+
+(* bl_getter is a function which returns a bl given an id *)
+let of_stree bl_getter st = 
+  let pt = Hashtbl.create (1+(Stree.max_id st)) in
+  (* righto is None if we have a pendant edge *)
+  let add_edge id left righto = 
+    let bl = bl_getter id in
+    safe_add pt id
+      (match righto with
+      | None -> Pend(id, bl, left)
+      | Some right -> Inte(bl, left, right))
+  in
+  let rec aux above_ids = function
+    | Stree.Node(id, tL) ->
+        let ids_of_tl = List.map Stree.top_id in
+        add_edge id above_ids (Some (ids_of_tl tL));
+        List.iter
+          (fun (below, rest) -> aux (id::(ids_of_tl rest)) below) 
+          (Base.pull_each_out tL)
+    | Stree.Leaf id -> add_edge id above_ids None
+  in
+  (* the basic tree builder, which doesn't take tricky rooting into account *)
+  let root_build (to_build, rest) = 
+    aux (List.map Stree.top_id rest) to_build 
+  in
+  let () = 
+    match st with
+    | Stree.Leaf _ -> ()
+    | Stree.Node(_, [t1; t2]) -> begin
+      (* tree with degree two rootings require some special care *)
+      (* first build the basic tree *)
+      List.iter root_build (Base.pull_each_out [t1; t2]);
+      let (eid1, eid2) = (Stree.top_id t1, Stree.top_id t2) in
+      match ((eid1,Hashtbl.find pt eid1), (eid2,Hashtbl.find pt eid2)) with
+      | ((id1, Inte(bl1,l1,r1)), (id2, Inte(bl2,l2,r2))) -> 
+          (* remove degree two node at root *)
+          let join1 = get_other_side [id2] l1 r1
+          and join2 = get_other_side [id1] l2 r2
+          in
+          (* make new internal edge *)
+          Hashtbl.replace pt id1 (Inte(bl1+.bl2, join1, join2));
+          (* clean out old edge *)
+          Hashtbl.remove pt id2;
+          (* reconnect things to new edge *)
+          List.iter 
+            (fun id -> 
+              Hashtbl.replace pt id 
+                (edgel_replace ~src:id2 ~dst:id1 (Hashtbl.find pt id)))
+            (join1 @ join2);
+      | _ -> raise (Not_implemented "rooted on pendant edge")
+      end
+    | Stree.Node(_, tL) -> List.iter root_build (Base.pull_each_out tL);
+  in
+  pt
+
+let of_gtree gt = 
+  let get_bl i = 
+    try (IntMap.find i gt.Gtree.bark_map)#get_bl with
+    | Not_found -> failwith "tree is missing branch lengths"
+  in
+  of_stree get_bl gt.Gtree.stree
+
+let of_string s = of_gtree (Newick.of_string s)
+let of_file s = of_gtree (Newick.of_file s)
+
+
+(* *** TO *** *)
+
+(* uuuuuugly! *)
+exception Found_inte of int
+let find_internal pt = 
+  try
+    Hashtbl.iter 
+      (fun i -> function 
+        | Inte(_,_,_) -> raise (Found_inte i) 
+        | _ -> ())
+      pt;
+    None
+  with
+  | Found_inte i -> Some i
+
+let to_gtree pt = 
+  (* start with an index bigger than anything in pt *)
+  print_endline "whooops, we have to take something bigger than any of the orig
+  ids";
+  let count = ref (Hashtbl.fold (fun i _ -> max i) pt 0) 
+  and m = ref IntMap.empty
+  in
+  let add_bark i bl nameo = 
+    m := 
+      IntMap.add i 
+        (new Newick_bark.newick_bark (`Of_bl_name_boot(Some bl, nameo, None)))
+        !m
+  in
+  let rec aux ~bad our_id = 
+    match Hashtbl.find pt our_id with
+    | Inte(bl,l,r) ->
+        incr count;
+        add_bark (!count) bl None;
+        let our_side = get_side_without bad l r in
+        Stree.Node(!count, List.map (aux ~bad:our_id) our_side)
+    | Pend(id,bl,_) -> add_bark id bl (Some (string_of_int id)); Stree.Leaf id
+  in
+  match find_internal pt with
+  | Some start_edge -> begin
+      match Hashtbl.find pt start_edge with
+      | Inte(bl,l,r) -> 
+          let stl = aux ~bad:(List.hd r) start_edge
+          and str = aux ~bad:(List.hd l) start_edge
+          in
+          add_bark (Stree.top_id stl) (bl/.2.) None;
+          add_bark (Stree.top_id str) (bl/.2.) None;
+          incr count;
+          Gtree.gtree (Stree.Node(!count, [stl;str])) !m
+      | Pend(_,_,_) -> assert(false)
+  end
+  | None ->
+      let st = 
+        Stree.Node 
+          (1 + !count,
+          Hashtbl.fold
+            (fun _ e l ->
+              match e with 
+              | Inte(_,_,_) -> assert false
+              | Pend(id,bl,_) -> 
+                  add_bark id bl (Some (string_of_int id));
+                  (Stree.Leaf id)::l)
+            pt
+            [])
+      in
+      Gtree.gtree st !m
+
+let to_stree pt = Gtree.get_stree (to_gtree pt)
