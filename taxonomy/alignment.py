b3199476225de690a07dd5e81b181633fc274111
Noah Hoffman <noah.hoffman@gmail.com>
2011-03-14 16:52:47 -0700

    temporary workaround for issue 16

--- taxonomy/alignment.py
+++ taxonomy/alignment.py
@@ -2,10 +2,10 @@
 import json, sys, os, string, argparse, subprocess, re
 from string import Template
 from Bio import SeqIO, AlignIO
-from Bio.Seq import Seq, SeqRecord
 
-
-
+# TODO: Fix error associated with SeqRecord; see issue https://github.com/fhcrc/taxtastic/issues#issue/16
+# from Bio.Seq import Seq, SeqRecord
+from Bio.Seq import Seq
 
 class Alignment(object):
     """
