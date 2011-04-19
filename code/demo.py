ef816ed94e844adcad4253831ffb15d7bac3273d
Aaron Gallagher <habnabit@gmail.com>
2011-04-19 16:44:24 -0700

    Adding a way of showing the discordance with a colored phyloxml tree.

--- code/demo.py
+++ code/demo.py
@@ -1,13 +1,24 @@
 from Bio import Phylo
+from Bio.Phylo.PhyloXML import BranchColor
+
+from StringIO import StringIO
 import collections
+import itertools
+import operator
 import algotax
 import pprint
 import refpkg
+import copy
 import sys
 
 def main(refpkg_path):
     rp = refpkg.Refpkg(refpkg_path)
-    tree = Phylo.read(rp.resource('tree_file', 'rU'), 'newick')
+
+    sio = StringIO()
+    Phylo.convert(rp.resource('tree_file', 'rU'), 'newick', sio, 'phyloxml')
+    sio.seek(0)
+    tree = Phylo.read(sio, 'phyloxml')
+
     rp.load_db()
     curs = rp.db.cursor()
     seq_colors = curs.execute("""
@@ -48,13 +59,24 @@ def main(refpkg_path):
         print '  badness: %d' % (badness,)
         results = algotax.walk(tree.root, metadata)
         print '  possibilities:'
+        counter = itertools.count()
         for k in sorted(results, key=results.get):
             print '    %d [%s]' % (len(results[k]), '; '.join(k) or '(none)')
             if not k:
                 continue
-            cut_nodes = set(tree.get_terminals()) - results[k]
-            for nodename in sorted(node.name for node in cut_nodes):
-                print '      %s' % (nodename)
+            # XXX find a better method of doing this
+            discordance_tree = copy.deepcopy(tree)
+            uncut_nodes = set(n.name for n in results[k])
+            for node in sorted(discordance_tree.get_terminals(),
+                    key=operator.attrgetter('name')):
+                if node.name in uncut_nodes:
+                    continue
+                print '      %s' % (node.name)
+                node.color = BranchColor(255, 0, 0)
+                node.width = 5
+            fname = 'discordance-%s-%d.xml' % (rank, next(counter))
+            Phylo.write(discordance_tree, fname, 'phyloxml')
+            print '      [discordance tree written to %s]' % (fname,)
         print '    %d [whole set]' % (len(clade_map),)
 
 if __name__ == '__main__':
