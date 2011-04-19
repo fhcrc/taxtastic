dfe620c9eba34621be62e4bf3e81ddc02ce6c9bf
Aaron Gallagher <habnabit@gmail.com>
2011-04-19 14:01:27 -0700

    Adding in a demonstration of algotax on reference packages.
    Also adding a minimal reference package parser.

--- code/demo.py
+++ code/demo.py
@@ -1,15 +1,56 @@
-from StringIO import StringIO
 from Bio import Phylo
+import collections
 import algotax
+import pprint
+import refpkg
+import sys
 
-def main():
-    tree_string = StringIO('((A,B),(A,(B,B)))')
-    tree = Phylo.read(tree_string, 'newick')
-    colors = {n: n.name for n in tree.get_terminals()}
-    print tree
-    metadata = algotax.color_clades(tree, colors)
-    print metadata
-    print algotax.walk(tree.root, metadata)
+def main(refpkg_path):
+    rp = refpkg.Refpkg(refpkg_path)
+    tree = Phylo.read(rp.resource('tree_file', 'rU'), 'newick')
+    rp.load_db()
+    curs = rp.db.cursor()
+    seq_colors = curs.execute("""
+        SELECT t.rank,
+               seqname,
+               t.tax_name
+        FROM   parents
+               JOIN taxa t
+                 ON t.tax_id = parent
+               JOIN sequences s
+                 ON s.tax_id = child
+    """)
+    rank_map = collections.defaultdict(list)
+    for rank, seq, color in seq_colors:
+        rank_map[rank].append((seq, color))
+
+    rank_order = curs.execute("""
+        SELECT   rank
+        FROM     ranks
+        ORDER BY rank_order ASC
+    """)
+
+    for rank, in rank_order:
+        seq_colors = rank_map[rank]
+        if not seq_colors:
+            continue
+        clade_map = {c.name: c for c in tree.get_terminals()}
+        colors = {clade_map[seq]: color for seq, color in seq_colors}
+        print 'calculating for %s (|coloring| is %d)' % (rank, len(colors))
+        metadata = algotax.color_clades(tree, colors)
+        badness = sum(len(cut) - 1
+            for node, cut in metadata.cut_colors.iteritems()
+            if len(cut) > 1 and node != tree.root)
+        if not badness:
+            print '  completely convex; skipping'
+            continue
+
+        print '  badness: %d' % (badness,)
+        results = algotax.walk(tree.root, metadata)
+        print '  possibilities:'
+        for k in sorted(results, key=results.get):
+            print '    %d [%s]' % (len(results[k]), '; '.join(k) or '(none)')
+        print '    %d [whole set]' % (len(clade_map),)
 
 if __name__ == '__main__':
-    main()
+    main(*sys.argv[1:])
