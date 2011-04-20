e3cf92d55943724ce61caaab410afb3ae4cc8a4e
Aaron Gallagher <habnabit@gmail.com>
2011-04-19 17:37:55 -0700

    Adding an option parser and real flags and such.

--- code/demo.py
+++ code/demo.py
@@ -3,16 +3,37 @@ from Bio.Phylo.PhyloXML import BranchColor
 
 from StringIO import StringIO
 import collections
+import functools
 import itertools
+import argparse
 import operator
-import algotax
+import os.path
 import pprint
-import refpkg
 import copy
 import sys
 
-def main(refpkg_path):
-    rp = refpkg.Refpkg(refpkg_path)
+import algotax
+import refpkg
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description='find discordance in a reference package')
+    parser.add_argument('refpkg', nargs=1,
+        help='the reference package to operate on')
+    parser.add_argument('-o', '--outdir',
+        help='if specified, write discordance trees in phyloxml format '
+        'to this directory')
+    parser.add_argument('-s', '--summary',
+        action='store_const', dest='verbosity', const=1, default=2,
+        help='show only a summary instead of the full output')
+
+    args = parser.parse_args(argv)
+    def out(level, msg='', *params):
+        if args.verbosity >= level:
+            print msg % params
+    p = functools.partial(out, 2)
+
+    rp = refpkg.Refpkg(args.refpkg[0])
 
     sio = StringIO()
     Phylo.convert(rp.resource('tree_file', 'rU'), 'newick', sio, 'phyloxml')
@@ -41,43 +62,60 @@ def main(refpkg_path):
         ORDER BY rank_order ASC
     """)
 
+    discordance_trees = []
     for rank, in rank_order:
         seq_colors = rank_map[rank]
         if not seq_colors:
             continue
         clade_map = {c.name: c for c in tree.get_terminals()}
         colors = {clade_map[seq]: color for seq, color in seq_colors}
-        print 'calculating for %s (|coloring| is %d)' % (rank, len(colors))
+        p('calculating for %s (|coloring| is %d)', rank, len(colors))
         metadata = algotax.color_clades(tree, colors)
         badness = sum(len(cut) - 1
             for node, cut in metadata.cut_colors.iteritems()
             if len(cut) > 1 and node != tree.root)
         if not badness:
-            print '  completely convex; skipping'
+            p('  completely convex; skipping')
             continue
 
-        print '  badness: %d' % (badness,)
+        p('  badness: %d', badness)
         results = algotax.walk(tree.root, metadata)
-        print '  possibilities:'
+        p('  possibilities:')
         counter = itertools.count()
+        discordance_tree = None
         for k in sorted(results, key=results.get):
-            print '    %d [%s]' % (len(results[k]), '; '.join(k) or '(none)')
+            p('    %d [%s]', len(results[k]), '; '.join(k) or '(none)')
             if not k:
                 continue
-            # XXX find a better method of doing this
-            discordance_tree = copy.deepcopy(tree)
-            uncut_nodes = set(n.name for n in results[k])
-            for node in sorted(discordance_tree.get_terminals(),
-                    key=operator.attrgetter('name')):
-                if node.name in uncut_nodes:
+
+            if args.outdir:
+                # XXX find a better method of doing this
+                discordance_tree = copy.deepcopy(tree)
+
+            cut_nodes = set(tree.get_terminals()) - results[k]
+            for node in sorted(cut_nodes, key=operator.attrgetter('name')):
+                p('      %s', node.name)
+
+                if not discordance_tree:
                     continue
-                print '      %s' % (node.name)
+                node = discordance_tree.find_any(name=node.name)
                 node.color = BranchColor(255, 0, 0)
                 node.width = 5
+
+            if not discordance_tree:
+                continue
             fname = 'discordance-%s-%d.xml' % (rank, next(counter))
-            Phylo.write(discordance_tree, fname, 'phyloxml')
-            print '      [discordance tree written to %s]' % (fname,)
-        print '    %d [whole set]' % (len(clade_map),)
+            Phylo.write(
+                discordance_tree, os.path.join(args.outdir, fname), 'phyloxml')
+            discordance_trees.append(fname)
+
+        p('    %d [whole set]', len(clade_map))
+    if discordance_trees:
+        if args.verbosity > 1:
+            out(1)
+        out(1, 'discordance trees written:')
+        for d in discordance_trees:
+            out(1, '  %s', d)
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    main(sys.argv[1:])
