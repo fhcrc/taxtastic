e7c366087f728f803c8c07c8aa1a5370b830e26c
Aaron Gallagher <habnabit@gmail.com>
2011-04-28 11:31:18 -0700

    Changing badgraph to output csv instead of using matplotlib.

--- code/badgraph.py
+++ code/badgraph.py
@@ -1,9 +1,8 @@
-import matplotlib.pyplot as plt
 from Bio import Phylo
-import numpy as np
 
 from collections import Counter, defaultdict
 import argparse
+import csv
 import sys
 
 import algotax
@@ -14,9 +13,14 @@ def main(argv):
         description='reroot a reference package')
     parser.add_argument('refpkg', nargs=1,
         help='the reference package to operate on')
+    parser.add_argument('-o', '--outfile',
+        type=argparse.FileType('w'), default='-',
+        help='the name of the csv file to write out')
 
     args = parser.parse_args(argv)
 
+    args.outfile = csv.writer(args.outfile)
+    args.outfile.writerow(['rank', 'name', 'leaves', 'edges'])
     rp = refpkg.Refpkg(args.refpkg[0])
     rp.load_db()
     with rp.resource('tree_file', 'rU') as fobj:
@@ -57,21 +61,15 @@ def main(argv):
         color_edge_counts = Counter()
         for edge_colors in metadata.cut_colors.itervalues():
             color_edge_counts += Counter(edge_colors)
-        counts = [(color, color_leaf_counts[color], color_edge_counts[color])
-            for color in
-            color_leaf_counts.viewkeys() | color_edge_counts.viewkeys()]
-        plot_color, plot_leaves, plot_edges = zip(*counts)
-        plt.scatter(plot_leaves, plot_edges, marker='o', alpha=.6)
-        ylim = max(max(plot_leaves) * 2, max(plot_edges)) * 1.1
-        xlim = ylim / 2
-        t = np.arange(0, xlim, .01)
-        slope = 2 * t - 2
-        plt.plot(t, slope)
-        plt.xlabel('leaves')
-        plt.ylabel('edges')
-        plt.axis([1, xlim, 0, ylim])
-        plt.savefig('%s.png' % rank.encode())
-        plt.clf()
+
+        for color in (color_leaf_counts.viewkeys() |
+                      color_edge_counts.viewkeys()):
+            args.outfile.writerow([
+                rank,
+                color,
+                color_leaf_counts[color],
+                color_edge_counts[color]
+            ])
 
 if __name__ == '__main__':
     main(sys.argv[1:])
