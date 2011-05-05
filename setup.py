66a144a64c019abbd27bf6690f8eb215b011c1a2
Aaron Gallagher <habnabit@gmail.com>
2011-05-05 14:38:20 -0700

    Fixing the setup.py to include all subpackages.
    Also removing cruft.

--- setup.py
+++ setup.py
@@ -3,23 +3,19 @@ Create unix package:    python setup.py sdist
 """
 
 from distutils.core import setup
-import os
-import sys
 import glob
 
-from taxtastic.__init__ import __version__
+from taxtastic import __version__
 
 # all files with .py extension in top level are assumed to be scripts
 scripts = ['taxit'] + list(set(glob.glob('*.py')) - set(['setup.py']))
-               
+
 params = {'author': 'Noah Hoffman',
           'author_email': 'ngh2@uw.edu',
           'description': 'Tools for taxonomic naming and annotation',
           'name': 'taxtastic',
-          'package_dir': {'taxtastic': 'taxtastic'},
-          'packages': ['taxtastic'],
+          'packages': ['taxtastic', 'taxtastic.scripts', 'taxtastic.subcommands'],
           'scripts': scripts,
-          # 'package_data':{'taxonomy': glob.glob('data/*')},
           'url': 'https://github.com/fhcrc/taxtastic',
           'version': __version__,
           'requires': ['Python (>= 2.7)', 'sqlalchemy']}
