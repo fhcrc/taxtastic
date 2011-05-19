64d14c26f332433b4e2821eb60428da27ef75329
Noah Hoffman <noah.hoffman@gmail.com>
2011-05-19 14:46:06 -0700

    utils.getlines respects inline comments

--- taxtastic/utils.py
+++ taxtastic/utils.py
@@ -87,14 +87,14 @@ def get_new_nodes(fname):
 def getlines(fname):
     """
     Returns iterator of whitespace-stripped lines in file, omitting
-    blank lines and lines beginning with '#'
+    blank lines, lines beginning with '#', and line contents following
+    the first '#' character.
     """
     
     with open(fname) as f:
         for line in f:
             if line.strip() and not line.startswith('#'):
-                yield line.strip()
-                
+                yield line.split('#', 1)[0].strip()
 
 def mkdir(dirpath, clobber = False):
     """
