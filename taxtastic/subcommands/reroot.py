4c64831f44b0a8486c29f2d2556ebd66ddd96f43
Aaron Gallagher <habnabit@gmail.com>
2011-08-09 11:58:33 -0700

    Updating `taxit reroot` to just call `rppr reroot`.

--- taxtastic/subcommands/reroot.py
+++ taxtastic/subcommands/reroot.py
@@ -1,11 +1,11 @@
 """Reroots a reference package"""
+import subprocess
 import tempfile
 import logging
+import shutil
 import os
 
-from Bio import Phylo
-
-from taxtastic import algotax, refpkg
+from taxtastic import refpkg
 
 log = logging.getLogger(__name__)
 
@@ -15,31 +15,23 @@ def build_parser(parser):
     parser.add_argument('-p', '--pretend',
         action='store_true', default=False,
         help="don't save the rerooted tree; just attempt the rerooting.")
-    parser.add_argument('--ignore-missing-sequences',
-        action='store_true', default=False,
-        help="ignore sequences found in the ref tree with no taxonomic information")
 
 def action(args):
-    log.info('loading reference package')
-    rp = refpkg.Refpkg(args.refpkg[0])
-    rp.load_db()
-    with rp.resource('tree_file', 'rU') as fobj:
-        tree = Phylo.read(fobj, 'newick')
-    log.info('rerooting')
-    root = algotax.reroot_from_rp(tree.root, rp, args.ignore_missing_sequences)
-    if log.isEnabledFor(logging.DEBUG):
-        log.debug('new root is %d steps from the old root',
-                  len(tree.get_path(root)))
-    tree.root_with_outgroup(root)
+    refpkg_name, = args.refpkg
+    rp = refpkg.Refpkg(refpkg_name)
+    fd, name = tempfile.mkstemp(dir=refpkg_name)
+    os.close(fd)
+    try:
+        subprocess.check_call(['rppr', 'reroot', '-c', refpkg_name, '-o', name])
+    except:
+        os.unlink(name)
+        raise
 
     if args.pretend:
+        os.unlink(name)
         return
-    log.info('saving reference package')
-    fd, name = tempfile.mkstemp(dir=args.refpkg[0])
-    with os.fdopen(fd, 'w') as fobj:
-        Phylo.write(tree, fobj, 'newick',
-                    branchlengths_only=True,
-                    format_branch_length='%0.6f')
-    os.rename(name, rp.resource_path('tree_file'))
-    rp.rehash('tree_file')
+
+    shutil.copystat(name, rp.resource_path('tree'))
+    os.rename(name, rp.resource_path('tree'))
+    rp.rehash('tree')
     rp.save()
