386b20957f41f73452efe4ad201ecd69271afe29
Fred Ross <fredross@washington.edu>
2011-09-13 08:36:49 -0700

    Added GPL to code

--- taxtastic/refpkg.py
+++ taxtastic/refpkg.py
@@ -6,6 +6,20 @@ reference packages for pplacer.
 
 Note that Refpkg objects are *NOT* thread safe!
 """
+# This file is part of taxtastic.
+#
+#    taxtastic is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    taxtastic is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with taxtastic.  If not, see <http://www.gnu.org/licenses/>.
 import contextlib
 from decorator import decorator
 import subprocess
--- taxtastic/subcommands/add_nodes.py
+++ taxtastic/subcommands/add_nodes.py
@@ -1,4 +1,18 @@
 """Add new nodes to a database containing a taxonomy."""
+# This file is part of taxtastic.
+#
+#    taxtastic is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    taxtastic is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with taxtastic.  If not, see <http://www.gnu.org/licenses/>.
 
 from taxtastic import ncbi
 from taxtastic.taxonomy import Taxonomy
