93fb5bbb3786fb2bada6be94deffc83d869ebcdc
Noah Hoffman <noah.hoffman@gmail.com>
2011-11-01 22:39:04 -0700

    taxit info is more useful

--- taxtastic/subcommands/info.py
+++ taxtastic/subcommands/info.py
@@ -19,6 +19,8 @@ Show information about reference packages.
 import logging
 import os.path
 import shutil
+import csv
+from collections import defaultdict
 
 from Bio import Phylo, SeqIO
 
@@ -29,29 +31,40 @@ log = logging.getLogger(__name__)
 def build_parser(parser):
     parser.add_argument('refpkg', action='store', metavar='refpkg',
                         help='the reference package to operate on')
-    parser.add_argument('-n', '--seq-names', action = 'store_true',
+    parser.add_argument('-n', '--seq-names', action = 'store_true', default = False,
                         help = 'print a list of sequence names')
+    parser.add_argument('-t', '--taxonomy', action = 'store_true', default = False,
+                        help = 'print a tally of sequences representing each taxon at rank RANK')
+    parser.add_argument('-r', '--rank', metavar = 'RANK', default = 'species',
+                        help = 'rank at which to describe the taxonomy [%(default)s]')
 
-def tree_names(pkg):
-    tree = Phylo.read(pkg.file_abspath('tree'), 'newick')    
-    return [x.name for x in tree.get_terminals()]
-    
+def describe_taxonomy(pkg, rank):
+    tally = defaultdict(int)
+    with open(pkg.file_abspath('taxonomy')) as taxtab, open(pkg.file_abspath('seq_info')) as seq_info:  
+        taxdict = {row['tax_id']: row for row in csv.DictReader(taxtab)}
+        taxdict[''] = {'tax_name':'undefined'}
+        for refseq in csv.DictReader(seq_info):
+            tally[taxdict[refseq['tax_id']][rank]] += 1
+
+    items = [(taxdict[tax_id]['tax_name'], count) for tax_id, count in tally.items()]
+    for name, count in sorted(items):
+        print name, count
+        
 def action(args):
     """
     Show information about reference packages.
     """
     log.info('loading reference package')
 
-    pkg = refpkg.Refpkg(args.refpkg)    
-    tnames = tree_names(pkg)
+    pkg = refpkg.Refpkg(args.refpkg)
 
-    with open(pkg.file_abspath('aln_sto')) as f:
-        seqs = SeqIO.parse(f, 'stockholm')
-        snames = [s.id for s in seqs]
-
-    if not set(tnames) == set(snames):
-        sys.exit('Error: sequence names in the Stockholm alignment and tree file differ.')
-        
+    with open(pkg.file_abspath('seq_info')) as seq_info:
+        snames = [row['seqname'] for row in csv.DictReader(seq_info)]
+    
     if args.seq_names:    
-        for name in tnames:
-            print name
+        print '\n'.join(snames)
+    elif args.taxonomy:
+        describe_taxonomy(pkg, args.rank)
+    else:
+        print 'number of sequences:', len(snames)
+        print 'package components\n', '\n'.join(sorted(pkg.file_keys())) 
