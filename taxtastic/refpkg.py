fab945ef7cbd14fa2c6fdc07f6b41699512342bd
Aaron Gallagher <habnabit@gmail.com>
2012-01-09 18:14:56 -0800

    Re-adding `Refpkg.load_db`, which mysteriously vanished.

--- taxtastic/refpkg.py
+++ taxtastic/refpkg.py
@@ -23,8 +23,10 @@ Note that Refpkg objects are *NOT* thread safe!
 import contextlib
 from decorator import decorator
 import subprocess
+import itertools
 import tempfile
 import hashlib
+import sqlite3
 import shutil
 import os
 import copy
@@ -130,6 +132,64 @@ def transaction(f, self, *args, **kwargs):
             self.current_transaction = None
 
 
+class NoAncestor(Exception):
+    pass
+
+class OnUpdate(object):
+    def __init__(self, proxied):
+        self.proxied = proxied
+        self.setter = None
+
+    def __get__(self, inst, cls):
+        if inst is None:
+            return self
+        return getattr(inst, self.proxied)
+
+    def __set__(self, inst, value):
+        if self.setter:
+            self.setter(inst, value)
+        setattr(inst, self.proxied, value)
+
+    def __call__(self, f):
+        self.setter = f
+        return self
+
+class _IntermediateTaxon(object):
+    def __init__(self, tax_id, parent, rank, tax_name):
+        self.children = set()
+        self.tax_id = tax_id
+        self.parent = parent
+        self.rank = rank
+        self.tax_name = tax_name
+
+    _parent = _adjacent_to = None
+
+    @OnUpdate('_parent')
+    def parent(self, p):
+        if self.parent is not None:
+            self.parent.children.discard(self)
+        if p is not None:
+            p.children.add(self)
+
+    @OnUpdate('_adjacent_to')
+    def adjacent_to(self, n):
+        if n is not None:
+            self.parent = n.parent
+
+    def iterate_children(self, on_pop=None, including_self=True):
+        search_stack = [(None, set([self]))]
+        while search_stack:
+            if not search_stack[-1][1]:
+                parent, _ = search_stack.pop()
+                if on_pop:
+                    on_pop(parent)
+                continue
+            node = search_stack[-1][1].pop()
+            if node is not self or including_self:
+                yield node
+            search_stack.append((node, node.children.copy()))
+
+
 class Refpkg(object):
     _manifest_name = 'CONTENTS.json'
 
@@ -175,6 +235,8 @@ class Refpkg(object):
         error = self.is_invalid()
         if error:
             raise ValueError("%s is not a valid RefPkg: %s" % (path, error))
+
+        self.db = None
 
     def _log(self, msg):
         """Set the log message for this operation.
@@ -599,3 +661,155 @@ class Refpkg(object):
                 return "phylo_model is not valid JSON."
 
         return False
+
+
+    def load_db(self):
+        """Load the taxonomy into a sqlite3 database.
+
+        This will set ``self.db`` to a sqlite3 database which contains all of
+        the taxonomic information in the reference package.
+        """
+        db = sqlite3.connect(':memory:')
+        curs = db.cursor()
+
+        curs.execute("""
+            CREATE TABLE ranks (
+              rank TEXT PRIMARY KEY NOT NULL,
+              rank_order INT
+            )
+        """)
+
+        curs.execute("""
+            CREATE TABLE taxa (
+              tax_id TEXT PRIMARY KEY NOT NULL,
+              tax_name TEXT NOT NULL,
+              rank TEXT REFERENCES ranks (rank) NOT NULL
+            )
+        """)
+
+        curs.execute("""
+            CREATE TABLE sequences (
+              seqname TEXT PRIMARY KEY NOT NULL,
+              tax_id TEXT REFERENCES taxa (tax_id) NOT NULL
+            )
+        """)
+
+        curs.execute("""
+            CREATE TABLE hierarchy (
+              tax_id TEXT REFERENCES taxa (tax_id) PRIMARY KEY NOT NULL,
+              lft INT NOT NULL UNIQUE,
+              rgt INT NOT NULL UNIQUE
+            )
+        """)
+
+        curs.execute("""
+            CREATE VIEW parents AS
+            SELECT h1.tax_id AS child,
+                   h2.tax_id AS parent
+            FROM   hierarchy h1
+                   JOIN hierarchy h2
+                     ON h1.lft BETWEEN h2.lft AND h2.rgt
+        """)
+
+        taxon_map = {}
+        reader = csv.DictReader(open(self.file_abspath('taxonomy'), 'rU'))
+        for row in reader:
+            parent = taxon_map.get(row['parent_id'])
+            taxon = _IntermediateTaxon(
+                row['tax_id'], parent, row['rank'], row['tax_name'])
+            taxon_map[taxon.tax_id] = taxon
+
+        counter = itertools.count(1).next
+        def on_pop(parent):
+            if parent is not None:
+                parent.rgt = counter()
+        for node in taxon_map['1'].iterate_children(on_pop=on_pop):
+            node.lft = counter()
+
+        curs.executemany("INSERT INTO ranks (rank_order, rank) VALUES (?, ?)",
+            enumerate(reader._fieldnames[4:]))
+        curs.executemany("INSERT INTO taxa VALUES (?, ?, ?)",
+            ((t.tax_id, t.tax_name, t.rank) for t in taxon_map.itervalues()))
+        curs.executemany("INSERT INTO hierarchy VALUES (?, ?, ?)",
+            ((t.tax_id, t.lft, t.rgt) for t in taxon_map.itervalues()))
+
+        reader = csv.DictReader(open(self.file_abspath('seq_info'), 'rU'))
+        curs.executemany("INSERT INTO sequences VALUES (?, ?)",
+            ((row['seqname'], row['tax_id']) for row in reader))
+
+        db.commit()
+        self.db = db
+
+    def most_recent_common_ancestor(self, *ts):
+        """Find the MRCA of some tax_ids.
+
+        Returns the MRCA of the specified tax_ids, or raises ``NoAncestor`` if
+        no ancestor of the specified tax_ids could be found.
+        """
+        if len(ts) > 200:
+            res = self._large_mrca(ts)
+        else:
+            res = self._small_mrca(ts)
+
+        if res:
+            (res,), = res
+        else:
+            raise NoAncestor()
+        return res
+
+    def _large_mrca(self, ts):
+        """Find the MRCA using a temporary table."""
+        cursor = self.db.cursor()
+
+        cursor.execute("""
+            DROP TABLE IF EXISTS _mrca_temp
+        """)
+
+        cursor.execute("""
+            CREATE TEMPORARY TABLE _mrca_temp(
+                child TEXT PRIMARY KEY REFERENCES taxa (tax_id) NOT NULL
+            )
+        """)
+
+        cursor.executemany("""
+            INSERT INTO _mrca_temp
+            VALUES (?)
+        """, ((tid,) for tid in ts))
+        cursor.execute("""
+            SELECT parent
+            FROM   _mrca_temp
+                   JOIN parents USING (child)
+                   JOIN taxa
+                     ON parent = taxa.tax_id
+                   JOIN ranks USING (rank)
+            GROUP  BY parent
+            HAVING COUNT(*) = ?
+            ORDER  BY rank_order DESC
+            LIMIT  1
+        """, (len(ts),))
+
+        return cursor.fetchall()
+
+    def _small_mrca(self, ts):
+        """Find a MRCA using query parameters.
+
+        This only supports a limited number of tax_ids; ``_large_mrca`` will
+        support an arbitrary number.
+        """
+        cursor = self.db.cursor()
+        qmarks = ', '.join('?' * len(ts))
+        cursor.execute("""
+            SELECT parent
+            FROM   parents
+                   JOIN taxa
+                     ON parent = taxa.tax_id
+                   JOIN ranks USING (rank)
+            WHERE  child IN (%s)
+            GROUP  BY parent
+            HAVING COUNT(*) = ?
+            ORDER  BY rank_order DESC
+            LIMIT  1
+        """ % qmarks, ts + (len(ts),))
+
+        return cursor.fetchall()
+
