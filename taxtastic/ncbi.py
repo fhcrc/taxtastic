39741e83365032e01d0f8027b9f842a0bcb47ef8
Noah Hoffman <noah.hoffman@gmail.com>
2012-03-12 16:10:16 -0700

    create index on names.is_classified

--- taxtastic/ncbi.py
+++ taxtastic/ncbi.py
@@ -77,9 +77,9 @@ CREATE INDEX nodes_rank ON nodes(rank);
 CREATE INDEX names_tax_id ON names(tax_id);
 CREATE INDEX names_tax_name ON names(tax_name);
 CREATE INDEX names_is_primary ON names(is_primary);
+CREATE INDEX names_is_classified ON names(is_classified);
 CREATE INDEX names_taxid_is_primary ON names(tax_id, is_primary);
 CREATE INDEX names_name_is_primary ON names(tax_name, is_primary);
-
 -- CREATE UNIQUE INDEX names_id_name ON names(tax_id, tax_name, is_primary);
 
 """
