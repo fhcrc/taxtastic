e5a3f52cfdb8f63d562849789f27dd42c29920ce
Noah Hoffman <noah.hoffman@gmail.com>
2013-04-22 14:00:11 -0700

    taxit composition learned options --taxtable and --seq-info

--- taxtastic/subcommands/composition.py
+++ taxtastic/subcommands/composition.py
@@ -26,31 +26,52 @@ from taxtastic import refpkg
 
 log = logging.getLogger(__name__)
 
+
 def build_parser(parser):
     parser.add_argument('refpkg', action='store', metavar='refpkg',
-                        help='the reference package to operate on')
-    parser.add_argument('-r', '--rank', default = 'species',
-                        help = 'rank at which to show composition [%(default)s]')
-    parser.add_argument('-o', '--outfile', default = sys.stdout, type = argparse.FileType('w'),
-                        help = 'rank at which to show composition. Use --rank=tax_id to show original classifications [%(default)s]')
+                        help='the reference package to operate on', nargs='?')
+    parser.add_argument('-t', '--taxonomy', metavar = 'csv file',
+                        help = 'Path to taxtable '
+                        '(ignored if refpkg is provided, required otherwise)')
+    parser.add_argument('-i', '--seq_info', metavar = 'csv file',
+                        help = 'Path to seq_info '
+                        '(ignored if refpkg is provided, required otherwise)')
+    parser.add_argument('-r', '--rank', default = 'species', metavar = 'RANK',
+                        help = 'show composition at RANK [%(default)s]')
+    parser.add_argument('-o', '--outfile', default = sys.stdout,
+                        type = argparse.FileType('w'),
+                        help = ('rank at which to show composition. Use '
+                                '--rank=tax_id to show original '
+                                'classifications [%(default)s]'))
 
 
 def action(args):
-    log.info('loading reference package')
 
-    pkg = refpkg.Refpkg(args.refpkg, create=False)
+    if args.refpkg:
+        log.info('loading reference package')
+        pkg = refpkg.Refpkg(args.refpkg, create=False)
+        taxonomy = pkg.file_abspath('taxonomy')
+        seq_info = pkg.file_abspath('seq_info')
+    else:
+        taxonomy = args.taxonomy
+        seq_info = args.seq_info
 
-    with open(pkg.file_abspath('taxonomy')) as f:
-        taxdict = {r['tax_id']:r for r in csv.DictReader(f)}
+        if taxonomy is None or seq_info is None:
+            sys.exit('Error: --taxonomy and --seq-info are '
+                     'required if refpkg is not provided.')
 
+    with open(taxonomy) as f:
+        taxdict = {r['tax_id']: r for r in csv.DictReader(f)}
+
+    unclassified = '<unclassified at this rank>'
     counts = Counter()
-    with open(pkg.file_abspath('seq_info')) as f:
+    with open(seq_info) as f:
         for row in csv.DictReader(f):
             # tax_id at the specified rank
             tax_id = taxdict[row['tax_id']][args.rank] if row['tax_id'] else ''
-            tax_name = taxdict[tax_id]['tax_name'] if tax_id else '<unclassified at this rank>'
+            tax_name = taxdict[tax_id]['tax_name'] if tax_id else unclassified
             counts[(tax_name, tax_id)] += 1
 
     writer = csv.writer(args.outfile)
-    writer.writerow(['tax_name','tax_id','count'])
-    writer.writerows(sorted((n, i, c) for (n, i),c in counts.items()))
+    writer.writerow(['tax_name', 'tax_id', 'count'])
+    writer.writerows(sorted((n, i, c) for (n, i), c in counts.items()))
