532789f31ca1bfcca36fbd54bfa53b96d45b35f4
Connor McCoy <cmccoy@fhcrc.org>
2013-10-31 11:39:23 -0700

    bump version for development

--- taxtastic/__init__.py
+++ taxtastic/__init__.py
@@ -14,10 +14,10 @@
 #    along with taxtastic.  If not, see <http://www.gnu.org/licenses/>.
 
 
-__version__ = '0.5.1'
-__version_info__ = (0, 5, 1)
+__version__ = '0.6.0-dev'
+__version_info__ = (0, 6, 0, 'dev')
 
-if '+' in __version__ or 'pre' in __version__:
+if '+' in __version__ or 'dev' in __version__:
     # Try to append the commit hash to the version
     try:
         import subprocess
