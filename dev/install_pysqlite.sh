979719919a3f2a5401f02660d64de0847f7029cd
Noah Hoffman <noah.hoffman@gmail.com>
2017-09-18 17:02:44 -0700

    use pip2 in dev/install_pysqlite.sh

--- dev/install_pysqlite.sh
+++ dev/install_pysqlite.sh
@@ -28,8 +28,8 @@ if [[ $sqlite_ok ]]; then
 fi
 
 rm -rf pysqlite-$pysqlite_version
-pip install -U pip
-pip download pysqlite==$pysqlite_version
+pip2 install -U pip
+pip2 download pysqlite==$pysqlite_version
 tar -xf pysqlite-$pysqlite_version.tar.gz
 cd pysqlite-$pysqlite_version
 
