fe6098d93574928ad97f7770e2d7212cd6287570
Noah Hoffman <noah.hoffman@gmail.com>
2017-12-20 22:29:52 -0800

    add absolute path to venv in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,11 @@ import datetime
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
+venv = '{venv}/lib/python{v.major}.{v.minor}/site-packages'.format(
+    venv=os.environ['VIRTUAL_ENV'],
+    v=sys.version_info)
+
+sys.path.insert(0, os.path.abspath(venv))
 sys.path.insert(0, os.path.abspath('..'))
 
 from taxtastic import __version__
